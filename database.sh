df01aa5477227c8a9f3416176720917ae1e7ae5b
kirurr <timon09df@gmail.com>
2025-01-17 21:09:46 +0300

    fixes yep

--- database.sh
+++ database.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 if [ ! -f "/var/www/storage/unreal-asset-store.db" ]; then
     echo "database not found, creating"
     sqlite3 "/var/www/storage/unreal-asset-store.db" ""
+    chmod -R 777 "/var/www/storage"
+    echo "making database file accessible for everyone" 
     echo "seeding database"
     php "/var/www/html/seed.php"
     echo "successfully seeded database"
